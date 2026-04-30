cd17355b9504de598c51b7e2ace73f5aa72fc5e3
ehsannikfarjam <ehsannikfarjam@example.com>
2026-04-30 10:27:04 -0500

    Initial commit: Set up Little Lemon React Native capstone project

---
+++ App.js
@@ -0,0 +1,4 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+// Main Entry Point setup
+export default function App() { return <NavigationContainer>{/* App Screens */}</NavigationContainer>; }
---
+++ README.md
@@ -0,0 +1,4 @@
+# Little Lemon Capstone - React Native
+
+Developed by ehsannikfarjam.
+This repository contains the complete Little Lemon Food Ordering App for the Meta React Native Capstone assignment.
